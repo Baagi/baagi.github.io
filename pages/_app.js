079497c4504fc2e4880c5b47681898f57eaece13
Bat Battur <batbattur@Bats-MacBook-Pro.local>
2021-09-03 21:52:07 -0700

    Added chakra provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { ChakraProvider, theme, CSSReset } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+    )
 }
 
 export default MyApp
